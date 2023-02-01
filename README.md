b50d76fa6f58e91c33e58b15ec76a4fb1e730161
Chelsea Ehimare <chelseaehimare01@gmail.com>
2023-02-01 22:16:20 +0000

    vue

---
+++ README.md
@@ -0,0 +1,5 @@
+# cst3145 individual coursework 1
+# Github Repository Link
+# https://github.com/chelsea-e/webApp
+# Github Page Link
+# https://chelsea-e.github.io/webApp/
---
+++ lessons.js
@@ -0,0 +1,92 @@
+var lessons =  [
+    {
+      id: 1,
+      subject: "Mathematics",
+      location: "Houston",
+      price: 85,
+      spaces: 5,
+      icon: "fas fa-calculator",
+      images: "img/1.jpg"
+    },
+    {
+      id: 2,
+      subject: "English",
+      location: "Scotland",
+      price: 115,
+      spaces: 5,
+      icon: "fas fa-sort-alpha-down",
+      images: "img/2.jpg"
+    },
+    {
+      id: 3,
+      subject: "Music",
+      location: "Winnipeg",
+      price: 45,
+      spaces: 5,
+      icon: "fas fa-guitar",
+      images: "img/3.png"
+    },
+    {
+      id: 4,
+      subject: "Art",
+      location: "Paris",
+      price: 50,
+      spaces: 5,
+      icon: "fas fa-paint-brush",
+      images: "img/4.jpeg"
+    },
+    {
+      id: 5,
+      subject: "Nutrition",
+      location: "Italy",
+      price: 30,
+      spaces: 5,
+      icon: "fab fa-apple",
+      images: "img/5.png"
+    },
+    {
+      id: 6,
+      subject: "Chemistry",
+      location: "Ohio",
+      price: 65,
+      spaces: 5,
+      icon: "fas fa-atom",
+      images: "img/6.jpg"
+    },
+    {
+      id: 7,
+      subject: "History",
+      location: "Germany",
+      price: 15,
+      spaces: 5,
+      icon: "fas fa-landmark",
+      images: "img/7.png"
+    },
+    {
+      id: 8,
+      subject: "Accounting",
+      location: "Texas",
+      price: 100,
+      spaces: 5,
+      icon: "fas fa-money-bill-alt",
+      images: "img/8.jpg"
+    },
+    {
+      id: 9,
+      subject: "Literature",
+      location: "Australia",
+      price: 90,
+      spaces: 5,
+      icon: "fas fa-book",
+      images: "img/9.jpg"
+    },
+    {
+      id: 10,
+      subject: "French",
+      location: "Japan",
+      price: 35,
+      spaces: 5,
+      icon: "fas fa-assistive-listening-systems",
+      images: "img/10.jpg"
+    },
+  ]
